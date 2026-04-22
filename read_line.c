c28ce465bc52ec736ba2eee29ad3f020075e0c5a
Massire <diabymassire93@gmail.com>
2026-04-22 13:51:41 +0000

    function to read the line

---
+++ read_line.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+
+char *read_line(void)
+{
+char *line = NULL;
+size_t len = 0;
+ssize_t nread;
+
+nread = getline(&line, &len, stdin);
+if (nread == -1)
+{
+free(line);
+return (NULL);
+}
+
+remove_newline(line);
+return (line);
+}
