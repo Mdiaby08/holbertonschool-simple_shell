47a82412f782403293da4db8cd6a4e8ead51b8e8
Massire <diabymassire93@gmail.com>
2026-04-22 14:43:38 +0000

    fixed bug

--- execute.c
+++ execute.c
@@ -6,7 +6,6 @@ void execute_command(char *line)
 {
 pid_t child = fork();
 int status;
-char *args[] = {line, NULL};
 
 if (child == -1)
 {
@@ -16,10 +15,11 @@ return;
 
 if (child == 0)
 {
-if (execve(line, args, environ) == -1)
 char *args[2];
 args[0] = line;
 args[1] = NULL;
+
+if (execve(line, args, environ) == -1)
 {
 perror("./shell");
 exit(1);
