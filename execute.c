eb73ce54795cbc1860273970b96c65c39e897466
Massire <diabymassire93@gmail.com>
2026-04-23 10:02:46 +0000

    path handling modified

--- execute.c
+++ execute.c
@@ -4,48 +4,53 @@ extern char **environ;
 
 void execute_command(char *line)
 {
-pid_t child = fork();
-int status;
-char **args;
+    pid_t child;
+    int status;
+    char **args;
+    char *cmd_path = NULL;
 
-if (child == -1)
-{
-perror("fork");
-return;
-}
+    args = split_line(line);
 
-if (child == 0)
-{
-args = split_line(line);
+    /* Si la commande contient un / -> tester directement */
+    if (strchr(args[0], '/'))
+    {
+        if (access(args[0], X_OK) != 0)
+        {
+            perror("./hsh");
+            return; /* NE PAS fork si la commande n'existe pas */
+        }
+        cmd_path = args[0];
+    }
+    else
+    {
+        /* Sinon -> chercher dans PATH */
+        cmd_path = find_path(args[0]);
+        if (!cmd_path)
+        {
+            perror("./hsh");
+            return; /* NE PAS fork si la commande n'existe pas */
+        }
+    }
 
-if (execve(line, args, environ) == -1)
-{
-perror("./shell");
-exit(1);
-}
-}
-else
-{
-wait(&status);
-}
-args = split_line(line);
+    /* Exécuter */
+    child = fork();
+    if (child == -1)
+    {
+        perror("fork");
+        return;
+    }
 
-if (strchr(args[0], '/'))
-{
-if (access(args[0], X_OK) != 0)
-{
-perror("./hsh");
-return;
-}
-cmd_path = args[0];
-}
-else
-{
-cmd_path = find_path(args[0]);
-if (!cmd_path)
-{
-perror("./hsh");
-return;
-}
-}
+    if (child == 0)
+    {
+        execve(cmd_path, args, environ);
+        perror("./hsh");
+        exit(1);
+    }
+    else
+    {
+        wait(&status);
+    }
+
+    if (cmd_path != args[0])
+        free(cmd_path);
 }
