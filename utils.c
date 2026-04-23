3c8a4c6a01930cc586ccbf3353770efdd0a2b55b
Massire <diabymassire93@gmail.com>
2026-04-23 15:38:46 +0000

    codind style modiefied

--- utils.c
+++ utils.c
@@ -3,30 +3,30 @@
 /* enlève le '\n' final de getline */
 void remove_newline(char *line)
 {
-    line[strcspn(line, "\n")] = '\0';
+line[strcspn(line, "\n")] = '\0';
 }
 
 /* découpe la ligne en mots (tokens) */
 char **split_line(char *line)
 {
-    char **args;
-    char *token;
-    int i = 0;
+char **args;
+char *token;
+int i = 0;
 
-    /* tableau de 64 mots max → suffisant pour simple_shell */
-    args = malloc(sizeof(char *) * 64);
-    if (!args)
-        return NULL;
+/* tableau de 64 mots max → suffisant pour simple_shell */
+args = malloc(sizeof(char *) * 64);
+if (!args)
+return NULL;
 
-    /* découper sur espace et tabulation */
-    token = strtok(line, " \t");
-    while (token)
-    {
-        args[i] = token;
-        i++;
-        token = strtok(NULL, " \t");
-    }
+/* découper sur espace et tabulation */
+token = strtok(line, " \t");
+while (token)
+{
+args[i] = token;
+i++;
+token = strtok(NULL, " \t");
+}
 
-    args[i] = NULL; /* fin du tableau */
-    return args;
+args[i] = NULL; /* fin du tableau */
+return args;
 }
