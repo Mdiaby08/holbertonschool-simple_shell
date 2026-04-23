dcbafd54151a0086d2359e874431236b12aae5f0
Massire <diabymassire93@gmail.com>
2026-04-23 08:57:07 +0000

    fixed utils.c coding style

--- utils.c
+++ utils.c
@@ -7,39 +7,39 @@ line[strcspn(line, "\n")] = '\0';
 
 void trim_spaces(char *line)
 {
-    int i = 0;
-    int j = strlen(line) - 1;
+int i = 0;
+int j = strlen(line) - 1;
 
-    while (line[i] == ' ' || line[i] == '\t')
-        i++;
+while (line[i] == ' ' || line[i] == '\t')
+i++;
 
-    while (j >= i && (line[j] == ' ' || line[j] == '\t'))
-        j--;
+while (j >= i && (line[j] == ' ' || line[j] == '\t'))
+j--;
 
-    line[j + 1] = '\0';
+line[j + 1] = '\0';
 
-    if (i > 0)
-        memmove(line, line + i, j - i + 2);
+if (i > 0)
+memmove(line, line + i, j - i + 2);
 }
 
 char **split_line(char *line)
 {
-    char **args;
-    char *token;
-    int i = 0;
+char **args;
+char *token;
+int i = 0;
 
-    args = malloc(sizeof(char *) * 64);
-    if (!args)
-        return (NULL);
+args = malloc(sizeof(char *) * 64);
+if (!args)
+return (NULL);
 
-    token = strtok(line, " ");
-    while (token)
-    {
-        args[i] = token;
-        i++;
-        token = strtok(NULL, " ");
-    }
-    args[i] = NULL;
+token = strtok(line, " ");
+while (token)
+{
+args[i] = token;
+i++;
+token = strtok(NULL, " ");
+}
+args[i] = NULL;
 
-    return (args);
+return (args);
 }
