5b530d28251e5a0c6b2c5b0519e4ba544814dac7
Massire <diabymassire93@gmail.com>
2026-04-23 15:35:16 +0000

    codind style modiefied and annotation

--- shell.c
+++ shell.c
@@ -1,42 +1,42 @@
 #include "shell.h"
  
 /**
- * main - entry point of the simple shell
- * @argc: argument count (unused)
- * @argv: argument vector, argv[0] is the program name
- *
- * Return: last command exit status
- */
+*main - entry point of the simple shell
+*@argc: argument count (unused)
+*@argv: argument vector, argv[0] is the program name
+*
+*Return: last command exit status
+*/
 int main(int argc, char **argv)
 {
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t nread;
-	int last_status = 0;
+char *line = NULL;
+size_t len = 0;
+ssize_t nread;
+int last_status = 0;
  
-	(void)argc;
+(void)argc;
  
-	while (1)
-	{
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "$ ", 2);
+while (1)
+{
+if (isatty(STDIN_FILENO))
+write(STDOUT_FILENO, "$ ", 2);
  
-		nread = getline(&line, &len, stdin);
+nread = getline(&line, &len, stdin);
  
-		if (nread == -1)
-		{
-			if (isatty(STDIN_FILENO))
-				write(STDOUT_FILENO, "\n", 1);
-			free(line);
-			return (last_status);
-		}
+if (nread == -1)
+{
+if (isatty(STDIN_FILENO))
+write(STDOUT_FILENO, "\n", 1);
+free(line);
+return (last_status);
+}
  
-		remove_newline(line);
+remove_newline(line);
  
-		last_status = execute_command(line, argv[0]);
-	}
+last_status = execute_command(line, argv[0]);
+}
  
-	free(line);
-	return (last_status);
+free(line);
+return (last_status);
 }
  
