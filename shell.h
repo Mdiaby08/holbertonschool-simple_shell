1a5b799ad0bf11bc89b9c967bb0531ae69f6a4a0
Massire <diabymassire93@gmail.com>
2026-04-24 01:18:33 +0000

    execute_command modfied

--- shell.h
+++ shell.h
@@ -8,24 +8,20 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-extern char **environ; /*tableau global des variables d'environnement */
+/* tableau global des variables d'environnement */
+extern char **environ;
 
 /* utils.c */
-void remove_newline(char *line);  /*retire le '\n' final */
-char **split_line(char *line);    /*découpe la ligne en mots */
+void remove_newline(char *line);      /* retire le '\n' final */
+char **split_line(char *line);        /* découpe la ligne en mots */
 
-/* read_line.c */
-ssize_t	read_line(char **line, size_t *len); /*lit une ligne depuis stdin */
+/* execute.c */
+void execute_command(char *line);     /* exécute une commande */
 
-/*prompt.c */
-void display_prompt(void); /* affiche "$ " si mode interactif */
-
-/*execute.c */
-int	execute_command(char *line, char *progname); /*lance une commande */
-
-/*path.c */
-char *find_path(char *command);   /*cherche la commande dans PATH */
-char *get_path_value(void);       /*retourne la valeur de PATH */
-char *build_path(char *dir, char *command); /*assemble "dir/commande" */
+/* path.c */
+char *find_path(char *command);       /* cherche la commande dans PATH */
+char *get_path_value(void);           /* récupère la valeur de PATH */
+char *build_path(char *dir, char *command); /* construit "dir/commande" */
 
 #endif /* SHELL_H */
+
