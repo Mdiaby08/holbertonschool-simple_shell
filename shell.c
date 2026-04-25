75582b5af04fad801d0b901eacef5a260eb13dab
Mdiaby08 <diabymassire93@gmail.com>
2026-04-26 01:03:36 +0200

    Update shell.c

--- shell.c
+++ shell.c
@@ -2,26 +2,33 @@
 
 int main(void)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
 
-    while (1)
-    {
-        if (isatty(STDIN_FILENO))
-            write(STDOUT_FILENO, "$ ", 2);
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "$ ", 2);
 
-        nread = getline(&line, &len, stdin);
-        if (nread == -1)
-        {
-            free(line);
-            return 0;
-        }
+		nread = getline(&line, &len, stdin);
+		if (nread == -1)
+		{
+			free(line);
+			return (0);
+		}
 
-        remove_newline(line);
-        execute_command(line);
-    }
+		remove_newline(line);
 
-    free(line);
-    return 0;
+		if (strcmp(line, "exit") == 0)
+		{
+			free(line);
+			exit(0);
+		}
+
+		execute_command(line);
+	}
+
+	free(line);
+	return (0);
 }
