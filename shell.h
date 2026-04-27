5e536a25738884b52115ead7e075a135f9510f11
Mdiaby08 <diabymassire93@gmail.com>
2026-04-27 10:50:12 +0200

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,6 +1,7 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+/* standard libraries */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -8,18 +9,18 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-extern char **environ;
+extern char **environ; /* global array of "NAME=value" environment strings */
 
 /* utils.c */
-void remove_newline(char *line);
-char **split_line(char *line);
+void	remove_newline(char *line);  /* replaces trailing '\n' with '\0' */
+char	**split_line(char *line);    /* splits line into a word array (NULL-terminated) */
 
 /* execute.c */
-int execute_command(char *line);
+int	execute_command(char *line); /* tokenizes, resolves path, forks and runs */
 
 /* path.c */
-char *find_path(char *command);
-char *get_path_value(void);
-char *build_path(char *dir, char *command);
+char	*find_path(char *command);   /* searches each PATH dir for the command */
+char	*get_path_value(void);       /* returns the value of PATH from environ */
+char	*build_path(char *dir, char *command); /* builds the string "dir/command" */
 
-#endif
+#endif /* SHELL_H */
